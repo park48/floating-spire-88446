a45f1d3a330b8bf04fd78407d0f228ec4e179faf
park48 <bluepark48@gmail.com>
2020-09-20 19:47:49 +0900

    search 0 hit

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -228,12 +228,13 @@ class PostsController extends Controller
             $user = User::Where('name','LIKE',"%{$keyword}%")->first();
             if($user){
               $user_id = $user->id;
+              $query->orWhere('user_id', 'LIKE', "%{$user_id}%");
             }
 
             $query->where('title', 'LIKE', "%{$keyword}%")
                 ->orWhere('body', 'LIKE', "%{$keyword}%")
-                ->orWhere('address', 'LIKE', "%{$keyword}%")
-                ->orWhere('user_id', 'LIKE', "%{$user_id}%");
+                ->orWhere('address', 'LIKE', "%{$keyword}%");
+
 
             // if($query == NULL){
             //
