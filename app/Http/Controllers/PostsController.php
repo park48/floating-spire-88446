b97aff21915e073ac70e63eac6dfb3805c5caa2c
park48 <bluepark48@gmail.com>
2020-09-20 15:54:36 +0900

    search with user_name

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -227,7 +227,7 @@ class PostsController extends Controller
         if (!empty($keyword)) {
             $query->where('title', 'LIKE', "%{$keyword}%")
                 ->orWhere('body', 'LIKE', "%{$keyword}%")
-                ->orWhere('address', 'LIKE', "%{$keyword}%");
+                ->orWhere('address', 'LIKE', "%{$keyword}%")
                 ->orWhere('user_id', 'LIKE', "%{$user_id}%");
                 // ->orWhere('$user_name', 'LIKE', "%{$keyword}%");
                 // usernameで検索させたいが、{{ $user_name }}がエラーがでる。
