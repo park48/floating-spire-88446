782e8bd9c26194c9a56dac3add95836fe06bc415
park48 <bluepark48@gmail.com>
2020-09-21 21:00:41 +0900

    change url

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -38,7 +38,7 @@
     <br>
     <div class="form-group row mt-2">
       <div class="col-md-8 offset-md-4">
-          <a href="/login/google" class="btn btn-secondary" role="button">
+          <a href="{{ url('/login/google') }}" class="btn btn-secondary" role="button">
               Google Login
           </a>
       </div>
